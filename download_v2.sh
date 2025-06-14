93c0084e45c3483ac1eb2d5eccc2fc503ba959f6
Ubuntu <ubuntu@ip-172-31-43-227.ec2.internal>
2025-06-14 07:17:51 +0000

    save change

---
+++ download_v2.sh
@@ -0,0 +1,11 @@
+
+
+
+mkdir ./checkpoints/
+mkdir ./checkpoints/ppt-v2_cn
+
+git lfs clone https://code.openxlab.org.cn/zhuangjunhao/PowerPaint_v2.git ./checkpoints/ppt-v2_cn/
+
+cd ./checkpoints/ppt-v2_cn/
+git lfs install
+git lfs pull
