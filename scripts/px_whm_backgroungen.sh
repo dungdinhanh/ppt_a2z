bee514f7d5b8c4340c12c4809a8136e468a38f05
Ubuntu <ubuntu@ip-172-31-43-227.ec2.internal>
2025-06-26 09:15:19 +0000

    save change

--- scripts/px_whm_backgroungen.sh
+++ scripts/px_whm_backgroungen.sh
@@ -2,6 +2,6 @@
 
 
 cmd="accelerate launch batch_inference_fgrm.py --meta /home/ubuntu/data/data_2D/px_wh_54k/record_blipcap_p5.csv --image_dir /home/ubuntu/data/data_2D/px_wh_54k/images --mask_dir /home/ubuntu/data/data_2D/px_wh_54k/images \
-    --output_dir /home/ubuntu/data/data_2D_processed/px_wh_54k/background/ --batch_size 7"
+    --output_dir /home/ubuntu/data/data_2D_processed/px_wh_54k/background/ --batch_size 15"
 echo ${cmd}
 eval ${cmd}
